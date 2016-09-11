00fbebbc5b3899deb43dfcbce5a8525567946a15
David Herdoiza <dav93herdoiza93@gmail.com>
2016-09-10 18:01:51 -0600

    write xml sriviendo

--- src/convert_afnd_to_afd/WriteXML.java
+++ src/convert_afnd_to_afd/WriteXML.java
@@ -5,16 +5,26 @@
  */
 package convert_afnd_to_afd;
 
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Attr;
+import javax.xml.transform.stream.StreamSource;
+import org.w3c.dom.Comment;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -24,6 +34,22 @@ import org.w3c.dom.Element;
  */
 public class WriteXML {
 
+    public static String prettyFormat(String input, int indent) {
+        try {
+            Source xmlInput = new StreamSource(new StringReader(input));
+            StringWriter stringWriter = new StringWriter();
+            StreamResult xmlOutput = new StreamResult(stringWriter);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute("indent-number", indent);
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(xmlInput, xmlOutput);
+            return xmlOutput.getWriter().toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e); // simple exception handling, please review it
+        }
+    }
+
     public static void writeXMLFile(AutomataDeterministico automata, String file) {
         try {
 
@@ -32,11 +58,14 @@ public class WriteXML {
 
             // root elements
             Document doc = docBuilder.newDocument();
+            Comment comment = doc.createComment("Created with JFLAP 6.4.");
+            doc.appendChild(comment);
+
             Element rootElement = doc.createElement("structure");
             doc.appendChild(rootElement);
 
             // type elements
-            Element type = doc.createElement("Type");
+            Element type = doc.createElement("type");
             type.appendChild(doc.createTextNode("fa"));
             rootElement.appendChild(type);
 
@@ -83,18 +112,32 @@ public class WriteXML {
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File(file + ".jff"));
+            StreamResult result2 = new StreamResult(new StringWriter());
 
-            // Output to console for testing
-            // StreamResult result = new StreamResult(System.out);
-            transformer.transform(source, result);
+            transformer.transform(source, result2);
 
-            System.out.println("File saved!");
+            String xmlString = result2.getWriter().toString();
+            xmlString = prettyFormat(xmlString, 4);
+            xmlString = xmlString.replaceAll("    ", "\t");
+            xmlString = xmlString.replaceAll("\r\n", "&#13;\n");
+            xmlString = xmlString.replaceAll("</structure>&#13;", "</structure>");
+            xmlString = xmlString.replaceAll("encoding=\"UTF-8\"", "encoding=\"UTF-8\" standalone=\"no\"");
+            System.out.println(xmlString);
+            BufferedWriter writer = null;
+            writer = new BufferedWriter( new FileWriter(file + ".jff"));
+            writer.write( xmlString);
+            if(writer != null)
+                writer.close();
+
 
         } catch (ParserConfigurationException pce) {
             pce.printStackTrace();
         } catch (TransformerException tfe) {
             tfe.printStackTrace();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(WriteXML.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(WriteXML.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
